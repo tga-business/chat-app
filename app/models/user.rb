3895cc097b22595b2d0aab40dc14ebe1dcfbd120
tga-business <81005306+tga-business@users.noreply.github.com>
2021-04-06 15:36:29 +0900

    room_userテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+  has_many :room_users
+  has_many :rooms, through: :room_users 
 end
+
+#throughは関連付けを意味するメソッド
